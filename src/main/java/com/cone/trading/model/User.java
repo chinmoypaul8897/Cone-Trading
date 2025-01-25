099ddedf11ee4e434ab7338d7c08fc13b5ddc9e0
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-01-26 00:06:32 +0530

    User Entity Created

---
+++ src/main/java/com/cone/trading/model/User.java
@@ -0,0 +1,24 @@
+package com.cone.trading.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id ;
+    private String fullName ;
+    private String email ;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password ;
+    private USER_ROLE role ;
+
+
+}
