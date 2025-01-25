abcd8383ad1dfaae2fa67ae6e0b1e1148246a9dd
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-01-26 00:21:30 +0530

    TwoFactor Authuntication entity created

--- src/main/java/com/cone/trading/model/TwoFactorAuth.java
+++ src/main/java/com/cone/trading/model/TwoFactorAuth.java
@@ -1,4 +1,11 @@
 package com.cone.trading.model;
 
+import lombok.Data;
+
+import java.lang.ref.PhantomReference;
+
+@Data
 public class TwoFactorAuth {
+    private boolean isEnabled = false ;
+    private VerificationType sendTo;
 }
