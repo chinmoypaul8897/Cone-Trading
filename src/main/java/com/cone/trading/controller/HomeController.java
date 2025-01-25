2d0e417ff1f6f622b2bb00f4215fc89d9749438a
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-01-26 00:00:52 +0530

    created home controller

---
+++ src/main/java/com/cone/trading/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.cone.trading.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping
+    public String home()
+    {
+        return "Welcome to cone trading ";
+    }
+}
