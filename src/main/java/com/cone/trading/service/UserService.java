aa93ab7ffec715b931b1c78b7372bd70cfa2e3a2
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-01-26 23:59:00 +0530

    User service implementation created

--- src/main/java/com/cone/trading/service/UserService.java
+++ src/main/java/com/cone/trading/service/UserService.java
@@ -1,15 +1,18 @@
 package com.cone.trading.service;
 
+import com.cone.trading.domain.VerificationType;
 import com.cone.trading.model.User;
 
 public interface UserService {
 
-    public User findUserProfileByJwt(String jwt);
-    public User findUserByEmail(String email);
-    public User findUserById (Long userId );
+    public User findUserProfileByJwt(String jwt) throws Exception;
+    public User findUserByEmail(String email) throws Exception;
+    public User findUserById (Long userId ) throws Exception;
 
 
-    public User enableTwoFactorAuthentication(User user);
+    public User enableTwoFactorAuthentication(
+            VerificationType verificationType ,
+            String sendTo , User user );
 
     User updatePassword (User user , String newPassword );
 
