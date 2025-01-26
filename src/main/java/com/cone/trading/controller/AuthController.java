472a5d29c7cdc664b775455f8c6ce3db7f7a64c6
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-01-26 17:50:56 +0530

    Jwt Provider created

--- src/main/java/com/cone/trading/controller/AuthController.java
+++ src/main/java/com/cone/trading/controller/AuthController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,16 +21,35 @@ public class AuthController {
     private UserRepository userRepository;
 
     @PostMapping("/signup")
-    public ResponseEntity<User> register( @RequestBody User user )
+    public ResponseEntity<User> register( @RequestBody User user ) throws Exception
     {
+
+        // email check
+        User isEmailExist = userRepository.findByEmail(user.getEmail());
+
+        if (isEmailExist != null )
+        {
+            throw new Exception("email is already used with another account ");
+        }
+
+        // new user create
         User newUser = new User();
         newUser.setEmail(user.getEmail());
         newUser.setPassword(user.getPassword());
         newUser.setEmail(user.getEmail());
         newUser.setFullName(user.getFullName());
 
-
+        // save new user data
         User savedUser = userRepository.save(newUser);
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                user.getEmail(),
+                user.getPassword()
+        );
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        // create jwt token
+
 
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
 
