b4e461f8107ad9abe75ee8ae5f4a6b07418ea26b
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-01-27 14:43:06 +0530

    CoinService created

---
+++ src/main/java/com/cone/trading/service/CoinService.java
@@ -0,0 +1,27 @@
+package com.cone.trading.service;
+
+import com.cone.trading.model.Coin;
+
+import java.util.List;
+
+public interface CoinService {
+
+    List<Coin> getCoinList(int page );
+
+    String getMarketChart(String coinId , int days );
+
+    String getCoinDetails (String coinId);
+
+    Coin findById (String coinId );
+
+    String searchCoin (String keyword);
+
+    String getTop50CoinsByMarketCapRank();
+
+
+    String GetTradingCoins();
+
+
+
+
+}
