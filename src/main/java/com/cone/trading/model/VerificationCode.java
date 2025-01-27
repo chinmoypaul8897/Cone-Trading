3fa10ac7c5bae8ddb4112d925080a4afda521c12
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-01-27 14:01:55 +0530

    Coin Api Creating

--- src/main/java/com/cone/trading/model/VerificationCode.java
+++ src/main/java/com/cone/trading/model/VerificationCode.java
@@ -6,8 +6,9 @@ import lombok.Data;
 
 import javax.naming.ldap.PagedResultsControl;
 
-@Entity
+
 @Data
+@Entity
 public class VerificationCode {
 
     @Id
@@ -23,6 +24,63 @@ public class VerificationCode {
     private String mobile ;
     private VerificationType verificationType;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getOtp() {
+        return otp;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public VerificationType getVerificationType() {
+        return verificationType;
+    }
+
+    public void setVerificationType(VerificationType verificationType) {
+        this.verificationType = verificationType;
+    }
+
+    @Override
+    public String toString() {
+        return "VerificationCode{" +
+                "id=" + id +
+                ", otp='" + otp + '\'' +
+                ", user=" + user +
+                ", email='" + email + '\'' +
+                ", mobile='" + mobile + '\'' +
+                ", verificationType=" + verificationType +
+                '}';
+    }
 }
