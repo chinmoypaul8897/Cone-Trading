d3d11d367cb028f61437bbfa00df8296e2b31ee8
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-01-29 00:58:43 +0530

    Payment Order Entity and PaymentOrderStatus created

---
+++ src/main/java/com/cone/trading/domain/PaymentOrderStatus.java
@@ -0,0 +1,7 @@
+package com.cone.trading.domain;
+
+public enum PaymentOrderStatus {
+    PENDING ,
+    SUCCESS ,
+    FAILED
+}
--- src/main/java/com/cone/trading/model/PaymentOrder.java
+++ src/main/java/com/cone/trading/model/PaymentOrder.java
@@ -1,6 +1,7 @@
 package com.cone.trading.model;
 
 
+import com.cone.trading.domain.PaymentOrderStatus;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
