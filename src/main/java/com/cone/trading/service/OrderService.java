785e1fc392b6f4e9c8f467d4b359600bad94844e
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-01-28 10:26:35 +0530

    OrderService created

---
+++ src/main/java/com/cone/trading/service/OrderService.java
@@ -0,0 +1,23 @@
+package com.cone.trading.service;
+
+import com.cone.trading.domain.OrderType;
+import com.cone.trading.model.Coin;
+import com.cone.trading.model.Order;
+import com.cone.trading.model.OrderItem;
+import com.cone.trading.model.User;
+
+import java.util.List;
+
+public interface OrderService {
+
+    Order createOrder (User user , OrderItem orderItem , OrderType orderType);
+
+    Order getOrderById (Long orderId );
+
+    List<Order> getAllOrderOfUser (Long  userId , OrderType orderType , String assetSymbol  );
+
+    Order processOrder (Coin coin , double quantity , OrderType orderType , User user );
+
+
+
+}
