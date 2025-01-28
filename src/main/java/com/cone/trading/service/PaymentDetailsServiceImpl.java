55fc2a58ab7fca7270a36f0363da79d77ef087cb
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-01-29 00:28:02 +0530

    PaymentDetailsService Implementation created

---
+++ src/main/java/com/cone/trading/service/PaymentDetailsServiceImpl.java
@@ -0,0 +1,18 @@
+package com.cone.trading.service;
+
+import com.cone.trading.model.PaymentDetails;
+import com.cone.trading.model.User;
+
+public class PaymentDetailsServiceImpl implements PaymentDetailsService{
+
+
+    @Override
+    public PaymentDetails addPaymentDetails(String accountNumber, String accountHolderName, String ifsc, String bankName, User user) {
+        return null;
+    }
+
+    @Override
+    public PaymentDetails getUsersPaymentDetails(User user) {
+        return null;
+    }
+}
