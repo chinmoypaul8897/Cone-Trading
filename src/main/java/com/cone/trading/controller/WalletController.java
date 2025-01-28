3e65aad506e53c672fd0dfdd377734a6b526108f
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-01-29 03:25:58 +0530

    Wallet Control Api completed

--- src/main/java/com/cone/trading/controller/WalletController.java
+++ src/main/java/com/cone/trading/controller/WalletController.java
@@ -1,10 +1,9 @@
 package com.cone.trading.controller;
 
-import com.cone.trading.model.Order;
-import com.cone.trading.model.User;
-import com.cone.trading.model.Wallet;
-import com.cone.trading.model.WalletTransaction;
+import com.cone.trading.model.*;
+import com.cone.trading.response.PaymentResponse;
 import com.cone.trading.service.OrderService;
+import com.cone.trading.service.PaymentService;
 import com.cone.trading.service.UserService;
 import com.cone.trading.service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +22,9 @@ public class WalletController {
 
     @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private PaymentService paymentService;
 
     @GetMapping("api/wallet")
     public ResponseEntity<Wallet> getUserWallet (@RequestHeader("Authorization") String jwt ) throws Exception {
@@ -60,6 +62,32 @@ public class WalletController {
         Order order = orderService.getOrderById(orderId);
 
         Wallet wallet = walletService.payOrderPayment(order,user);
+
+        return new ResponseEntity<>(wallet,HttpStatus.ACCEPTED);
+    }
+
+
+    @PutMapping("/api/wallet/deposit")
+    public ResponseEntity<Wallet> addBalanceToWallet (
+            @RequestHeader("Authorization") String jwt ,
+            @RequestParam(name = "order_id") Long orderId ,
+            @RequestParam(name = "payment_id") String paymentId )
+            throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+
+
+        Wallet wallet = walletService.getUserWallet(user);
+
+        PaymentOrder order = paymentService.getPaymentOrderById(orderId);
+
+        Boolean status = paymentService.ProccedPaymentOrderById(order,paymentId);
+
+        if (status)
+        {
+            wallet =walletService.addBalance(wallet,order.getAmount());
+
+        }
+
 
         return new ResponseEntity<>(wallet,HttpStatus.ACCEPTED);
     }
--- src/main/java/com/cone/trading/model/PaymentOrder.java
+++ src/main/java/com/cone/trading/model/PaymentOrder.java
@@ -23,4 +23,58 @@ public class PaymentOrder {
 
     @ManyToOne
     private User user;
+
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public PaymentOrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PaymentOrderStatus status) {
+        this.status = status;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "PaymentOrder{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", status=" + status +
+                ", paymentMethod=" + paymentMethod +
+                ", user=" + user +
+                '}';
+    }
 }
