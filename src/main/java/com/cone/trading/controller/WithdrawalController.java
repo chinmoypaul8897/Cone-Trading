9c3fc0183e7234d5550c48474b91dc8959fbe3fb
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-01-28 20:24:47 +0530

    WithdrawalController created

---
+++ src/main/java/com/cone/trading/controller/WithdrawalController.java
@@ -0,0 +1,102 @@
+package com.cone.trading.controller;
+
+import com.cone.trading.model.User;
+import com.cone.trading.model.Wallet;
+import com.cone.trading.model.WalletTransaction;
+import com.cone.trading.model.Withdrawal;
+import com.cone.trading.service.UserService;
+import com.cone.trading.service.WalletService;
+import com.cone.trading.service.WithdrawalService;
+import com.cone.trading.service.WithdrawalServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
+public class WithdrawalController {
+
+    @Autowired
+    private WithdrawalService withdrawalService;
+
+    @Autowired
+    private WalletService walletService;
+
+    @Autowired
+    private UserService userService;
+
+//    @Autowired
+//    private WalletTransactionService walletTransactionService;
+
+    @PostMapping("/api/withdrawal/{amount}")
+    public ResponseEntity<?> withdrawalRequest(
+            @PathVariable Long amount ,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception
+    {
+        User user = userService.findUserProfileByJwt(jwt);
+        Wallet userWallet = walletService.getUserWallet(user);
+
+        Withdrawal withdrawal = withdrawalService.reqestWithdrawal(amount,user);
+        walletService.addBalance(userWallet,-withdrawal.getAmount());
+
+
+       // WalletTransaction walletTransaction = walletTransactionService.createTransaction();
+
+
+        return new ResponseEntity<>(withdrawal, HttpStatus.OK);
+
+    }
+
+    @PatchMapping("api/admin/withdrawal/{id}/proceed/{accept}")
+    public ResponseEntity<?> proceedWithdrawal(
+        @PathVariable Long id ,
+        @PathVariable boolean accept ,
+        @RequestHeader("Authorization") String jwt ) throws Exception
+    {
+        User user = userService.findUserProfileByJwt(jwt);
+
+        Withdrawal withdrawal = withdrawalService.proceedWithdrawal(id,accept);
+
+        Wallet userWallet = walletService.getUserWallet(user);
+
+        if (!accept)
+        {
+            walletService.addBalance(userWallet,withdrawal.getAmount());
+        }
+
+        return new ResponseEntity<>(withdrawal,HttpStatus.OK);
+
+    }
+
+    @GetMapping("api/withdrawal")
+    public ResponseEntity<List<Withdrawal>> getWithdrawalHistory(
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception
+    {
+        User user = userService.findUserProfileByJwt(jwt);
+
+        List<Withdrawal> withdrawal = withdrawalService.getUsersWithdrawalHistory(user);
+
+        return new ResponseEntity<>(withdrawal,HttpStatus.OK);
+    }
+
+
+    @GetMapping("/api/admin/withdrawal")
+    public ResponseEntity<List<Withdrawal>> getAllWithdrawalRequest(
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception
+    {
+        User user = userService.findUserProfileByJwt(jwt);
+
+        List<Withdrawal> withdrawal = withdrawalService.getAllWithdrawalRequest();
+
+        return new ResponseEntity<>(withdrawal,HttpStatus.OK);
+    }
+
+
+
+}
