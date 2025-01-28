c3224acbd27244e1fad12c8ec5e8bddb66b3d0a1
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-01-29 01:00:57 +0530

    Payment Method Created

---
+++ src/main/java/com/cone/trading/domain/PaymentMethod.java
@@ -0,0 +1,6 @@
+package com.cone.trading.domain;
+
+public enum PaymentMethod {
+    RAZORPAY,
+    STRIPE
+}
--- src/main/java/com/cone/trading/model/PaymentOrder.java
+++ src/main/java/com/cone/trading/model/PaymentOrder.java
@@ -1,11 +1,10 @@
 package com.cone.trading.model;
 
 
+import com.cone.trading.domain.PaymentMethod;
 import com.cone.trading.domain.PaymentOrderStatus;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.core.Base64Variant;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -19,4 +18,9 @@ public class PaymentOrder {
     private Long amount ;
 
     private PaymentOrderStatus status;
+
+    private PaymentMethod paymentMethod;
+
+    @ManyToOne
+    private User user;
 }
